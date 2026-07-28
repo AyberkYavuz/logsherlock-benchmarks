24c1783c48c3a9060c52fc04dde5b35ecef9ac28
AyberkYavuz <ayberk.yavuz06@gmail.com>
2026-07-28 16:04:14 +0300

    scenario.ts file was added

---
+++ apps/typescript-pino/src/scenario/scenario.ts
@@ -0,0 +1,6 @@
+export enum Scenario {
+  NORMAL = "normal",
+  PAYMENT_PROVIDER_DOWN = "payment_provider_down",
+  PAYMENT_TIMEOUT = "payment_timeout",
+  NO_ROOMS_AVAILABLE = "no_rooms_available",
+}
