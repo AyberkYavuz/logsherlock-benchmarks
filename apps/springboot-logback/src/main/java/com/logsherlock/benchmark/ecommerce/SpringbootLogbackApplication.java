dc8a460646489a63e95ecf02eec5cf9ea5a9e0a8
AyberkYavuz <ayberk.yavuz06@gmail.com>
2026-07-30 13:53:15 +0300

    SpringbootLogbackApplication.java file was added

---
+++ apps/springboot-logback/src/main/java/com/logsherlock/benchmark/ecommerce/SpringbootLogbackApplication.java
@@ -0,0 +1,8 @@
+@SpringBootApplication
+public class SpringbootLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLogbackApplication.class, args);
+    }
+
+}
