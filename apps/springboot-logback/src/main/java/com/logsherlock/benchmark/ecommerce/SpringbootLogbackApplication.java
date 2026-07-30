374fe7b140c45666c9e175eaa0d5cad109366475
AyberkYavuz <ayberk.yavuz06@gmail.com>
2026-07-30 14:05:04 +0300

    SpringbootLogbackApplication.java file update

--- apps/springboot-logback/src/main/java/com/logsherlock/benchmark/ecommerce/SpringbootLogbackApplication.java
+++ apps/springboot-logback/src/main/java/com/logsherlock/benchmark/ecommerce/SpringbootLogbackApplication.java
@@ -1,3 +1,8 @@
+package com.logsherlock.benchmark.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SpringbootLogbackApplication {
 
